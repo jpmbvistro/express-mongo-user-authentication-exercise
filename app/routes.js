4022fbc337e4c6ebb27d203c391ceefde9f2e950
Juan <jpmbvistro@gmail.com>
2021-04-13 07:03:02 -0400

    f

--- app/routes.js
+++ app/routes.js
@@ -27,6 +27,8 @@ module.exports = function(app, passport, db) {
 // message board routes ===============================================================
 
     app.post('/messages', (req, res) => {
+      console.log('+++++++++++++++++TEST+++++++++++++++++')
+      console.log(req.user)
       db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
         if (err) return console.log(err)
         console.log('saved to database')
